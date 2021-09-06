154b8ae9df3c2e6438d330036257fa39f08ae489
Antonio Savage <asavage@bisontrails.co>
2021-09-06 16:38:56 -0400

    new contracts, starting the gold script

---
+++ contracts.js
@@ -0,0 +1,7 @@
+const contracts = {
+  rarity: "0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb",
+  gold: "0x2069B76Afe6b734Fb65D1d099E7ec64ee9CC76B2",
+  attributes: "0xB5F5AF1087A8DA62A23b08C00C6ec9af21F397a1",
+};
+
+module.exports = contracts;
--- levelUp.js
+++ levelUp.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const ethers = require("ethers");
 const { NonceManager } = require("@ethersproject/experimental");
-const contractAddress = "0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb";
-
+const contracts = require("./contracts");
+const rarityContractAddress = contracts.rarity;
 const rarityAbi = require("./abis/rarity.json");
 const endpoint = process.env.FTMPROVIDER; // eslint-disable-line no-undef
 
@@ -10,7 +10,11 @@ const provider = new ethers.providers.JsonRpcProvider(endpoint, 250);
 const Wallet = new ethers.Wallet(process.env.PRIVATE_KEY); // eslint-disable-line no-undef
 const wallet = Wallet.connect(provider);
 const nonceManager = new NonceManager(wallet);
-const contract = new ethers.Contract(contractAddress, rarityAbi, provider);
+const contract = new ethers.Contract(
+  rarityContractAddress,
+  rarityAbi,
+  provider
+);
 const writeContract = contract.connect(nonceManager);
 const summonerIds = require("./summoners");
 
